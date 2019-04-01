90c024f2f6bcf5a351c610c66ca7a0384577eb40
Sean Warren <s.warren@garvan.org.au>
2019-04-01 12:01:00 +1100

    Force submodule update

--- add_ui_common.m
+++ add_ui_common.m
@@ -2,6 +2,8 @@ function add_ui_common
     % Update submodule if required
     if ~exist('matlab-ui-common','dir')
         system('git submodule update --init --recursive')
+    else
+        system('git submodule update')      
     end
     
     addpath('matlab-ui-common')
